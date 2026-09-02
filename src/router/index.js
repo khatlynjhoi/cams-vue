b68f544f0e0979513dd90a16e65aaf2e0aca4c01
ICT HEAD <k.j.cruz@bma.edu.ph>
2026-09-02 15:33:38 +0800

    fix: switch router to web hash history for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import DashboardView from '../views/DashboardView.vue'
 import QuestionBankView from '../views/QuestionBankView.vue'
@@ -59,7 +59,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
